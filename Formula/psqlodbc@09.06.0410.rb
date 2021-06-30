93df525caa2a9b6915b2a2c4749da7c460c215c7
Kit Wong <kit@art19.com>
2021-06-29 19:11:37 -0500

    update with brew style --fix

--- Formula/psqlodbc@09.06.0410.rb
+++ Formula/psqlodbc@09.06.0410.rb
@@ -5,16 +5,15 @@ class PsqlodbcAT09060410 < Formula
   sha256 "965b74587a2fd09368acff340ed3bdf2e831cee4e78d1caa79d0cc621364a88c"
 
   bottle do
-    cellar :any
-    sha256 "ea65d3c1ab051fe19fd431704ac5a1f4602d6e9103f199866d3965982ebbd0f8" => :sierra
-    sha256 "f6f472ae0d6cb4022c3a7900dd7dbae0617e4a8317f542349efc9ee84089e849" => :el_capitan
-    sha256 "851118d307bdefb65b07e789f2770809c9e3f76f93ca570e256bacabe9bb353b" => :yosemite
+    sha256 cellar: :any, sierra:     "ea65d3c1ab051fe19fd431704ac5a1f4602d6e9103f199866d3965982ebbd0f8"
+    sha256 cellar: :any, el_capitan: "f6f472ae0d6cb4022c3a7900dd7dbae0617e4a8317f542349efc9ee84089e849"
+    sha256 cellar: :any, yosemite:   "851118d307bdefb65b07e789f2770809c9e3f76f93ca570e256bacabe9bb353b"
   end
 
   head do
     url "https://git.postgresql.org/git/psqlodbc.git"
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
